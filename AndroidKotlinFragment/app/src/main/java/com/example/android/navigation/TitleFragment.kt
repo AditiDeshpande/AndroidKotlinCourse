db4f3d28404c3dc360994a836ed5474afade408d
aditi <aditid11@gmail.com>
2020-12-21 15:46:19 +0530

    Added Fragment to the activity using layout. Statically.

---
+++ AndroidKotlinFragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,37 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+    /*
+    To make a Frag compile , u need to create a binding object and
+    inflate the Fragment's view (Which is equivalent to using setContentView()
+    for an Activity)
+    */
+
+        /*
+        A binding class is generated for each layout file. By default
+        , the name of the class is based on the name layout file
+        converting it to Pascal case and adding Binding suffix to it.
+        Our layout filename is fragment_title so the corresponding
+        generated class is FragmentTitleBinding. This class holds all
+        the bindings from the layout properties to the layout Views
+        and knows how to assign values for the binding expressions.
+         */
+
+        val binding =  DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+        R.layout.fragment_title , container , false)
+        return binding.root
+    }
+
+
+}
